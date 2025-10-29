e37bdd0bc2cedb01de819ac7233797f683126ff3
Sebastian Kullander <sebastian.kullander@protonmail.com>
2025-10-29 20:13:50 +0100

    minor styling fixes for main menu, removed some unused files

--- components/sections/carousel/Card.tsx
+++ components/sections/carousel/Card.tsx
@@ -27,13 +27,15 @@ export default function Card({ id, title, text, tags, githubUrl }: CardProps ) {
                     </svg>
                     <p>View Live</p>
                 </div>
-                <div className="flex flex-row gap-2 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" viewBox="0 0 256 256">
+                <div className="flex flex-row gap-2 items-center cursor-pointer group">
+                    <svg className="group-hover:scale-115 transition-transform duration-200" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" viewBox="0 0 256 256">
                         <path d="M216,104v8a56.06,56.06,0,0,1-48.44,55.47A39.8,39.8,0,0,1,176,192v40a8,8,0,0,1-8,8H104a8,8,0,0,1-8-8V216H72a40,40,0,0,1-40-40A24,24,0,0,0,8,152a8,8,0,0,1,0-16,40,40,0,0,1,40,40,24,24,0,0,0,24,24H96v-8a39.8,39.8,0,0,1,8.44-24.53A56.06,56.06,0,0,1,56,112v-8a58.14,58.14,0,0,1,7.69-28.32A59.78,59.78,0,0,1,69.07,28,8,8,0,0,1,76,24a59.75,59.75,0,0,1,48,24h24a59.75,59.75,0,0,1,48-24,8,8,0,0,1,6.93,4,59.74,59.74,0,0,1,5.37,47.68A58,58,0,0,1,216,104Z"></path>
                     </svg>
-                    <a href={githubUrl} target="_blank" rel="noopener noreferrer" className="cursor-pointer">
-                        View on Github
-                    </a>
+                    <div>
+                        <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+                            View on Github
+                        </a>
+                    </div>
                 </div>
             </div>
             <div className="text-[10px] sm:text-sm flex flex-wrap gap-2 text-white">
--- components/ui/menu/BackdropMenuContent.tsx
+++ components/ui/menu/BackdropMenuContent.tsx
@@ -8,68 +8,36 @@ const menuItems = ["Top", "About", "Projects", "Contact"];
 
 export default function BackdropMenuContent({ ref }: { ref?: RefObject<HTMLDivElement | null>}) {
     return (
-        <div ref={ref} className="flex flex-col w-full h-full">
+        <div ref={ref} className="flex flex-col w-full h-full p-8">
             <div className="relative h-full">
                 <div  className="w-full fixed top-1/2 -translate-y-1/2 text-white font-medium xl:gap-2 site-x-padding">
-                    <div className="w-full flex flex-row justify-between">
-                        <div>
-                            <div className="flex flex-row gap-4 overflow-hidden leading-35 text-6xl">
-                                <h1 className="special-heading innerMenuItem">Sections</h1>
-                            </div>
+                    <div className="w-full flex flex-row">
+                        <div className="pl-[64px] sm:pl-[98px] md:pl-[128px] lg: xl:pl-[192px]">
                             {menuItems.map((item) => (
                                 <div key={item} className="overflow-hidden flex flex-row items-center gap-4 cursor-pointer group">
-                                    <Image src={svgStar} alt="" width={40} height={40} className=" absolute scale-0 group-hover:scale-100 transition-transform duration-300" />
-                                    <h2 className="innerMenuItem p-2 text-5xl">{item}</h2>
+                                    <Image src={svgStar} alt="" width={40} height={40} className=" scale-0 group-hover:scale-100 transition-transform duration-300" />
+                                    <h2 className="innerMenuItem p-2 text-[80px]/25 leading-27">{item}</h2>
                                 </div>
                             ))}
-                        </div>
-                        <div>
-                            <div className="">
-                                <div className="overflow-hidden">
-                                    <h1 className="special-heading outerMenuItem leading-35 text-6xl">Pages</h1>
-                                </div>
-                                <div className="overflow-hidden">
-                                    <p className="outerMenuItem p-2 text-5xl">My stack</p>
-                                </div>
-                                <div className="overflow-hidden">
-                                    <p className="outerMenuItem p-2 text-5xl">Contact</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className="overflow-hidden">
-                                <h1 className="special-heading text-6xl leading-35">Other links</h1>
-                            </div>
-                            <div className="overflow-hidden">
-                                <div className="flex flex-row outerMenuItem">
-                                    <p className=" p-2 text-5xl">My Blog</p>
-                                    <div className="relative">
-                                        <p className="absolute whitespace-nowrap uppercase tracking-widest text-sm">Coming soon</p>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                     
                 </div>
             </div>
 
-
-
             <div className="w-full site-x-padding site-y-padding site-text-size text-white flex flex-row justify-between items-center">
-                
                 <div className="overflow-hidden flex flex-row gap-3">
                     <Link href="https://github.com/SebastianKullander9" target="_blank">
                         <div className="outerMenuItem flex flex-row group cursor-pointer items-center">
-                            <Image src={arrowRight} width={28} height={28} alt="" className="invert group-hover:scale-130 transition-transform duration-200" />
                             <p>GitHub</p>
+                            <Image src={arrowRight} width={34} height={34} alt="" className="invert group-hover:scale-120 transition-transform duration-200" />
                         </div>
                     </Link>
                     <p className="outerMenuItem"></p>
                     <Link href="https://www.linkedin.com/in/sebastian-kullander-9922b9133/" target="_blank">
                         <div className="outerMenuItem flex flex-row group cursor-pointer items-center">
-                            <Image src={arrowRight} width={28} height={28} alt="" className="invert group-hover:scale-130 transition-transform duration-200" />
                             <p>LinkedIn</p>
+                            <Image src={arrowRight} width={34} height={34} alt="" className="invert group-hover:scale-120 transition-transform duration-200" />
                         </div>
                     </Link>
                 </div>
